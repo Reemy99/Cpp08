458288a37457d684f515e045486e59a8853f4ec0
Reem Aldahhan <realdahh@lab1r1s9.42abudhabi.ae>
2023-09-07 13:31:50 +0400

    finished

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	Span sp1 = Span(5); // the size of the vector
+	Span sp1 = Span(5);
 	try
 	{
 		sp1.addNumber(1);
@@ -11,31 +11,25 @@ int main()
 
 		std::cout << BLUE << "addNumber : shortest span : " << sp1.shortestSpan() << RESET << std::endl;
 		std::cout << BLUE << "addNumber : longest span  : " << sp1.longestSpan() << RESET << std::endl;
-
-    std::cout << "________________________" << std::endl;
-
+    	std::cout << "________________________" << std::endl;
 
 		std::srand(time(NULL));
+		Span sp2 = Span(10); 
 		std::vector<int> vec;
 
-		for (size_t i = 0; i < 10000; i++)
-		{
-			int n = std::rand() % 10000;
+		for (size_t i = 0; i < 10; i++){
+			int n = std::rand() % 10;
 			vec.push_back(n);
 		}
-		Span sp2 = Span(10000); 
+		sp2.addNumber(vec.begin(), vec.end());
 
-		sp2.addNumber(vec.begin() + vec.size() / 2, vec.end());
-
-		std::cout << BLUE <<"addNumber with iterators : shortest span "  << sp2.shortestSpan() << RESET << std::endl;
-
-		for (size_t i = 0; i < sp2._vec.size(); i++)
-		{
-			// std::cout << sp2._vec[i] << " ";
+		for (size_t i = 0; i < sp2._vec.size(); i++) {
+			 std::cout << sp2._vec[i] << " ";
 		}
+		std::cout << BLUE <<"addNumber with iterators : shortest span "  << sp2.shortestSpan() << RESET << std::endl;
 		std::cout << BLUE <<"addNumber with iterators : longest span " << sp2.longestSpan() << RESET << std::endl;
     }
-    catch (const std::exception &e){
+    catch (const std::exception &e) {
 		std::cerr << RED << e.what() << RESET << std::endl;
     }
 	return 0;
